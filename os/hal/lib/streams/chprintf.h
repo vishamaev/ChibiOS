3022e23b4d14965e1c37000f4b65f8b1d0f4abdd
Fabien Poussin <fabien.poussin@gmail.com>
2017-03-31 15:37:21 +0200

    Replacing chprintf by our custom one, renaming current one.

--- os/hal/lib/streams/chprintf.h
+++ os/hal/lib/streams/chprintf.h
@@ -1,5 +1,5 @@
 /*
-    ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio
+    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -26,6 +26,7 @@
 #define _CHPRINTF_H_
 
 #include <stdarg.h>
+#include <math.h>
 
 /**
  * @brief   Float type support.
@@ -37,12 +38,42 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-  int chvprintf(BaseSequentialStream *chp, const char *fmt, va_list ap);
-  int chprintf(BaseSequentialStream *chp, const char *fmt, ...);
-  int chsnprintf(char *str, size_t size, const char *fmt, ...);
+  void chvprintf(BaseSequentialStream *chp, const char *fmt, va_list ap);
+  char *ftoa(char *p, double num, unsigned long precision);
 #ifdef __cplusplus
 }
 #endif
+
+/**
+ * @brief   System formatted output function.
+ * @details This function implements a minimal @p printf() like functionality
+ *          with output on a @p BaseSequentialStream.
+ *          The general parameters format is: %[-][width|*][.precision|*][l|L]p.
+ *          The following parameter types (p) are supported:
+ *          - <b>x</b> hexadecimal integer.
+ *          - <b>X</b> hexadecimal long.
+ *          - <b>o</b> octal integer.
+ *          - <b>O</b> octal long.
+ *          - <b>d</b> decimal signed integer.
+ *          - <b>D</b> decimal signed long.
+ *          - <b>u</b> decimal unsigned integer.
+ *          - <b>U</b> decimal unsigned long.
+ *          - <b>c</b> character.
+ *          - <b>s</b> string.
+ *          .
+ *
+ * @param[in] chp       pointer to a @p BaseSequentialStream implementing object
+ * @param[in] fmt       formatting string
+ *
+ * @api
+ */
+static inline void chprintf(BaseSequentialStream *chp, const char *fmt, ...) {
+  va_list ap;
+
+  va_start(ap, fmt);
+  chvprintf(chp, fmt, ap);
+  va_end(ap);
+}
 
 #endif /* _CHPRINTF_H_ */
 
