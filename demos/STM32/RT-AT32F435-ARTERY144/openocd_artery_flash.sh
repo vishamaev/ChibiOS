51d27826a324acdeb80cd0164b7ba7e1124c4c49
Andrey Gusakov <dron0gus@gmail.com>
2023-06-07 23:36:33 +0300

    Add flashing script

--- demos/STM32/RT-AT32F435-ARTERY144/openocd_artery_flash.sh
+++ demos/STM32/RT-AT32F435-ARTERY144/openocd_artery_flash.sh
@@ -1,1 +1,8 @@
-./openocd_artery.sh -c "program build/ch.bin verify reset exit 0x08000000"
+#!/bin/sh
+openocd -f interface/cmsis-dap.cfg -f ./at32f4x.cfg \
+	-c "init" \
+	-c "halt" \
+	-c "flash write_image erase build/ch.elf" \
+	-c "flash verify_image build/ch.elf" \
+	-c "reset" \
+	-c "exit"
--- demos/STM32/RT-AT32F435-ARTERY144/openocd_artery_rtos.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-openocd -f interface/cmsis-dap.cfg -f ./at32f4x.cfg -c '$_TARGETNAME configure -rtos auto' $*
-
